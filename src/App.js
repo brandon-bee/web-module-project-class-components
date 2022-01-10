88a5b10a6053001b59365faf6295146e10dd6120
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-01-10 15:46:31 -0800

    Achieved MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,78 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+
+const todos = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: todos
+    };
+  }
+  // Add todo
+  handleAddTodo = (todoName) => {
+    const newTodo = {
+      task: todoName,
+      id: Date.now(),
+      completed: false
+    };
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTodo]
+    })
+  }
+
+  // Toggle completed
+  handleToggleTodo = (selectedTodo) => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(item => {
+        if(item.id === selectedTodo.id) {
+          return({
+            ...item,
+            completed: !item.completed
+          })
+        } else {
+          return item;
+        }
+      })
+    });
+  }
+
+  // Clear completed
+  handleClearTodos = () => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter(item => {
+        return !item.completed
+      })
+    })
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Todo List: MVP</h1>
+        <TodoList handleToggleTodo={this.handleToggleTodo} todos={this.state.todos} />
+        <TodoForm handleAddTodo={this.handleAddTodo} />
+        <button onClick={this.handleClearTodos}>Clear completed tasks</button>
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,16 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo from "./Todo";
+
+class TodoList extends React.Component {
+  render() {
+    return(
+      <div className="todo-list">
+        {this.props.todos.map(item => (
+          <Todo handleToggleTodo={this.props.handleToggleTodo} todo={item} key={item.id} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default TodoList;
